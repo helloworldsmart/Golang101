190fa8625f5eb3e8dd1f0abd7a9822d657a9f759
MichaelChen <helloworldsmart3@gmail.com>
2023-11-26 19:49:31 +0800

    feat: ch5

--- learning-go-book/ch5/playground.go
+++ learning-go-book/ch5/playground.go
@@ -1,10 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	divMain()
-	myFuncMain()
+	//divMain()
+	//myFuncMain()
+	//addToMain()
+	divAndRemainderMain()
 }
 
 func divMain() {
@@ -38,6 +44,39 @@ type MyFuncOpts struct {
 
 func MyFunc(opts MyFuncOpts) {
 	// Do Something
+}
+
+func addToMain() {
+	fmt.Println(addTo(3))
+	fmt.Println(addTo(3, 2))
+	fmt.Println(addTo(3, 2, 4, 6, 8))
+	a := []int{4, 3}
+	fmt.Println(addTo(3, a...))
+	fmt.Println(addTo(3, []int{1, 2, 3, 4, 5}...))
+}
+
+func addTo(base int, vals ...int) []int {
+	out := make([]int, 0, len(vals))
+	for _, v := range vals {
+		out = append(out, base+v)
+	}
+	return out
+}
+
+func divAndRemainderMain() {
+	result, remainder, err := divAndRemainder(5, 2)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(result, remainder)
+}
+
+func divAndRemainder(numerator int, denominator int) (int, int, error) {
+	if denominator == 0 {
+		return 0, 0, errors.New("cannot divide by zero")
+	}
+	return numerator / denominator, numerator % denominator, nil
 }
 
 func tutorial() {
