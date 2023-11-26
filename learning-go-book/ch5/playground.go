efe8f2bbaff7cfc7f437639a2962c6a5d258103a
MichaelChen <helloworldsmart3@gmail.com>
2023-11-26 19:07:44 +0800

    feat: ch5

--- learning-go-book/ch5/playground.go
+++ learning-go-book/ch5/playground.go
@@ -1,7 +1,43 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	divMain()
+	myFuncMain()
+}
+
+func divMain() {
+	result := div(5, 2)
+	fmt.Println(result)
+}
+
+func div(numerator int, denominator int) int {
+	if denominator == 0 {
+		return 0
+	}
+	return numerator / denominator
+}
+
+func myFuncMain() {
+	MyFunc(MyFuncOpts{
+		LastName: "Patel",
+		Age:      50,
+	})
+	MyFunc(MyFuncOpts{
+		FirstName: "Joe",
+		LastName:  "Smith",
+	})
+}
+
+type MyFuncOpts struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func MyFunc(opts MyFuncOpts) {
+	// Do Something
 }
 
 func tutorial() {
