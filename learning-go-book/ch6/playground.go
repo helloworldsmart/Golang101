6013cf493cb29890dc218a2bbe4fec45b289595b
MichaelChen <helloworldsmart3@gmail.com>
2023-11-30 22:59:04 +0800

    feat: ch 6

--- learning-go-book/ch6/playground.go
+++ learning-go-book/ch6/playground.go
@@ -1,7 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//tutorial2()
+	tutorial4()
 }
 
 func tutorial() {
@@ -10,4 +13,29 @@ func tutorial() {
 	//pointerX := &x
 	//pointerY := &y
 	//var pointerZ *string
+}
+
+func tutorial2() {
+	x := 10
+	pointerToX := &x
+	fmt.Println(pointerToX)
+	fmt.Println(*pointerToX)
+	z := 5 + *pointerToX
+	fmt.Println(z)
+}
+
+func tutorial3() {
+	var x *int
+	fmt.Println(x == nil)
+	fmt.Println(*x)
+	// panic: runtime error: invalid memory address or nil pointer dereference
+	// [signal SIGSEGV: segmentation violation code=0x1 addr=0x0 pc=0x1086e98]
+}
+
+func tutorial4() {
+	x := 10
+	var pointerToX *int
+	pointerToX = &x
+	fmt.Println(pointerToX)
+	fmt.Println(*pointerToX)
 }
