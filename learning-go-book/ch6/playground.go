b283fad360aecb8caa62723446eb1c333ad73f10
MichaelChen <helloworldsmart3@gmail.com>
2023-12-02 21:41:19 +0800

    feat: ch 6 指標

--- learning-go-book/ch6/playground.go
+++ learning-go-book/ch6/playground.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	//tutorial2()
-	tutorial4()
+	//tutorial4()
+	//tutorial5()
+	tutorial6()
 }
 
 func tutorial() {
@@ -38,4 +40,31 @@ func tutorial4() {
 	pointerToX = &x
 	fmt.Println(pointerToX)
 	fmt.Println(*pointerToX)
+}
+
+func tutorial5() {
+	var x = new(int)
+	fmt.Println(x == nil)
+	fmt.Println(*x)
+}
+
+type person struct {
+	FirstName  string
+	MiddleName *string
+	LastName   string
+}
+
+func tutorial6() {
+	p := person{
+		FirstName: "Pat",
+		//MiddleName: "Perry", // '"Perry"' (type string) cannot be represented by the type *string
+		//MiddleName: &"Perry", // Cannot take the address of '"Perry"'
+		MiddleName: stringp("Perry"),
+		LastName:   "Peterson",
+	}
+	fmt.Println(p)
+}
+
+func stringp(s string) *string {
+	return &s
 }
