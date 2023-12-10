a21d6c6ebcc252e809590499c532d7997b6a152d
MichaelChen <helloworldsmart3@gmail.com>
2023-12-10 20:21:36 +0800

    feat: ch 7

--- learning-go-book/ch7/playground.go
+++ learning-go-book/ch7/playground.go
@@ -1,7 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 }
 
 func tutorial() {
+}
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+type Score int
+type Converter func(string) Score
+type TeamScores map[string]Score
+
+func (p Person) String() string {
+	return fmt.Sprintf("%s %s, age %d", p.FirstName, p.LastName, p.Age)
+}
+
+type Counter struct {
+	total       int
+	lastUpdated time.Time
+}
+
+func (c *Counter) Increment() {
+	c.total++
+	c.lastUpdated = time.Now()
+}
+
+func (c Counter) String() string {
+	return fmt.Sprintf("total: %d, last updated: %v", c.total, c.lastUpdated)
 }
