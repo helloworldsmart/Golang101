2f0647647178691f57daf994c4ee3edcc1112bb0
MichaelChen <helloworldsmart3@gmail.com>
2023-12-10 21:37:53 +0800

    feat: ch 7 add IntTree

--- learning-go-book/ch7/playground.go
+++ learning-go-book/ch7/playground.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-}
-
-func tutorial() {
+	//tutorial()
+	//tutorial1()
+	tutorial2()
 }
 
 type Person struct {
@@ -37,4 +37,69 @@ func (c *Counter) Increment() {
 
 func (c Counter) String() string {
 	return fmt.Sprintf("total: %d, last updated: %v", c.total, c.lastUpdated)
+}
+
+func tutorial() {
+	var c Counter
+	fmt.Println(c.String())
+	c.Increment()
+	fmt.Println(c.String())
+}
+
+func doUpdateWrong(c Counter) {
+	c.Increment()
+	fmt.Println("in doUpdateWrong:", c.String())
+}
+
+func doUpdateRight(c *Counter) {
+	c.Increment()
+	fmt.Println("in doUpdateRight:", c.String())
+}
+
+func tutorial1() {
+	var c Counter
+	doUpdateWrong(c)
+	fmt.Println("in main:", c.String())
+	doUpdateRight(&c)
+	fmt.Println("in main:", c.String())
+}
+
+type IntTree struct {
+	val         int
+	left, right *IntTree
+}
+
+func (it *IntTree) Insert(val int) *IntTree {
+	if it == nil {
+		return &IntTree{val: val}
+	}
+	if val < it.val {
+		it.left = it.left.Insert(val)
+	} else if val > it.val {
+		it.right = it.right.Insert(val)
+	}
+	return it
+}
+
+func (it *IntTree) Contains(val int) bool {
+	switch {
+	case it == nil:
+		return false
+	case val < it.val:
+		return it.left.Contains(val)
+	case val > it.val:
+		return it.right.Contains(val)
+	default:
+		return true
+	}
+}
+
+func tutorial2() {
+	var it *IntTree
+	it = it.Insert(5)
+	it = it.Insert(3)
+	it = it.Insert(10)
+	it = it.Insert(2)
+	fmt.Println(it.Contains(2))  // true
+	fmt.Println(it.Contains(12)) // false
 }
