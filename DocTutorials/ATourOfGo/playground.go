3bdb615107ec7ffa3e22b47f2105cf0fb37eccb1
MichaelChen <helloworldsmart3@gmail.com>
2023-12-14 17:03:24 +0800

    feat:  A Tour Of Go , If and else

--- DocTutorials/ATourOfGo/playground.go
+++ DocTutorials/ATourOfGo/playground.go
@@ -73,7 +73,10 @@ func tutorial5() {
 func pow(x, n, lim float64) float64 {
 	if v := math.Pow(x, n); v < lim {
 		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
 	}
+	// can't use v here, though
 	return lim
 }
 
