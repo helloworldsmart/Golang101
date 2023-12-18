e944315c79aeb79aba4e23b013e9687c15cffb7e
MichaelChen <helloworldsmart3@gmail.com>
2023-12-18 22:28:54 +0800

    feat: 用 golang + orm + postgresql 產生  ERD 案例練習

--- Tutorials/OrderBooksSystem/main.go
+++ Tutorials/OrderBooksSystem/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Book struct {
+	gorm.Model
+	Title       string
+	Author      string
+	ISBN        string
+	PublishDate string
+	Price       string
+}
+
+type Customer struct {
+	gorm.Model
+	Name    string
+	Email   string
+	Photo   string
+	Address string
+}
+
+type Order struct {
+	gorm.Model
+	CustomerID uint
+	Total      float64
+}
+
+type OrderDetail struct {
+	gorm.Model
+	OrderID   uint
+	BookID    int
+	Quantity  int
+	UnitPrice float64
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// TODO:
+	dsn := "host=localhost port=5432 user=youruser dbname=yourdb sslmode=disable password=yourpassword"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// 自動遷移模式
+	db.AutoMigrate(&Book{}, &Customer{}, &Order{}, &OrderDetail{})
+
 }
