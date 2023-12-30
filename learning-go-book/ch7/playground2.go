a7227f239488723c4310ef18db9329efc215505a
MichaelChen <helloworldsmart3@gmail.com>
2023-12-30 13:55:16 +0800

    feat: interface

--- learning-go-book/ch7/playground2.go
+++ learning-go-book/ch7/playground2.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 	tutorial7()
+	tutorial8()
 }
 
 type Inner2 struct {
@@ -37,4 +38,32 @@ func tutorial7() {
 		S: "Hello",
 	}
 	fmt.Println(o.Double())
+}
+
+type LogicProvider struct {
+}
+
+func (lp LogicProvider) Process(data string) string {
+	// 商業邏輯
+	return ""
+}
+
+type Logic interface {
+	Process(data string) string
+}
+
+type Client struct {
+	L Logic
+}
+
+func (c Client) Program() {
+	// 從某處取得資料
+	c.L.Process(data)
+}
+
+func tutorial8() {
+	c := Client{
+		L: LogicProvider{},
+	}
+	c.Program()
 }
