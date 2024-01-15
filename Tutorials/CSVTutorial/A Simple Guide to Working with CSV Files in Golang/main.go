1b7345fbd2c84d37c224feb7da15deae2cae3aef
MichaelChen <helloworldsmart3@gmail.com>
2024-01-15 22:05:58 +0800

    feat:  Append to CSV Files

--- Tutorials/CSVTutorial/A Simple Guide to Working with CSV Files in Golang/main.go
+++ Tutorials/CSVTutorial/A Simple Guide to Working with CSV Files in Golang/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
 	"log"
 	"os"
 )
@@ -65,8 +66,37 @@ import (
 //}
 
 // MARK: Writing CSV Files
+//func main() {
+//	file, err := os.Create("data3.csv")
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	defer file.Close()
+//
+//	writer := csv.NewWriter(file)
+//	defer writer.Flush()
+//
+//	data := [][]string{
+//		{"name", "age", "city", "job", "company", "phone"},
+//		{"Kevin", "30", "New York", "Manager", "Folio Inc.", "355-025-0525"},
+//		{"John", "25", "Los Angeles", "Developer", "Folio Inc.", "555-230-2139"},
+//		{"Mary", "28", "Chicago", "HR", "Folio Inc.", "331-359-8311"},
+//		{"Elizabeth", "35", "New York", "Data Analyst", "Apple Inc.", "113-602-9009"},
+//	}
+//
+//	for _, value := range data {
+//		writer.Write(value)
+//	}
+//
+//	err = writer.WriteAll(data)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//}
+
+// MARK: Append to CSV Files
 func main() {
-	file, err := os.Create("data3.csv")
+	file, err := os.OpenFile("data3.csv", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -76,19 +106,14 @@ func main() {
 	defer writer.Flush()
 
 	data := [][]string{
-		{"name", "age", "city", "job", "company", "phone"},
-		{"Kevin", "30", "New York", "Manager", "Folio Inc.", "355-025-0525"},
-		{"John", "25", "Los Angeles", "Developer", "Folio Inc.", "555-230-2139"},
-		{"Mary", "28", "Chicago", "HR", "Folio Inc.", "331-359-8311"},
-		{"Elizabeth", "35", "New York", "Data Analyst", "Apple Inc.", "113-602-9009"},
-	}
-
-	for _, value := range data {
-		writer.Write(value)
+		{"Felix", "23", "Berlin", "Researcher", "PWC", "555-230-2139"},
+		{"Linda", "28", "Chicago", "Data Engineere", "KPMG", "331-359-8311"},
 	}
 
 	err = writer.WriteAll(data)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("Data appended to csv file successfully.")
 }
