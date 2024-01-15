32fa657ff3e739277de2ccccd0f12c11f56411aa
MichaelChen <helloworldsmart3@gmail.com>
2024-01-15 22:51:01 +0800

    feat: Open the CSV file

---
+++ Tutorials/CSVTutorial/How To Work With CSV Files In Go/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Open the CSV file
+	file, err := os.Open("data.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Read the CSV data
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = -1 // Allow variable number of fields
+	data, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	// Print the CSV data
+	for _, row := range data {
+		for _, col := range row {
+			fmt.Printf("%s,", col)
+		}
+		fmt.Println()
+	}
+}
