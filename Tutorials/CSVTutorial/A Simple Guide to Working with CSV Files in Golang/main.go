353d1bf0ccff7b4297964b9e8d3a9de3d4e184b0
MichaelChen <helloworldsmart3@gmail.com>
2024-01-15 21:42:03 +0800

    feat:  Writing CSV Files

--- Tutorials/CSVTutorial/A Simple Guide to Working with CSV Files in Golang/main.go
+++ Tutorials/CSVTutorial/A Simple Guide to Working with CSV Files in Golang/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/csv"
-	"fmt"
 	"log"
 	"os"
 )
@@ -43,24 +42,53 @@ import (
 //}
 
 // MARK: Reading files with different delimiters
+//func main() {
+//	file, err := os.Open("data2.csv")
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	defer file.Close()
+//
+//	reader := csv.NewReader(file)
+//	reader.FieldsPerRecord = -1
+//
+//	reader.Comma = '|'
+//
+//	csvData, err := reader.ReadAll()
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	for _, each := range csvData {
+//		fmt.Println(each)
+//	}
+//}
+
+// MARK: Writing CSV Files
 func main() {
-	file, err := os.Open("data2.csv")
+	file, err := os.Create("data3.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	reader := csv.NewReader(file)
-	reader.FieldsPerRecord = -1
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
 
-	reader.Comma = '|'
-
-	csvData, err := reader.ReadAll()
-	if err != nil {
-		log.Fatal(err)
+	data := [][]string{
+		{"name", "age", "city", "job", "company", "phone"},
+		{"Kevin", "30", "New York", "Manager", "Folio Inc.", "355-025-0525"},
+		{"John", "25", "Los Angeles", "Developer", "Folio Inc.", "555-230-2139"},
+		{"Mary", "28", "Chicago", "HR", "Folio Inc.", "331-359-8311"},
+		{"Elizabeth", "35", "New York", "Data Analyst", "Apple Inc.", "113-602-9009"},
 	}
 
-	for _, each := range csvData {
-		fmt.Println(each)
+	for _, value := range data {
+		writer.Write(value)
+	}
+
+	err = writer.WriteAll(data)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
