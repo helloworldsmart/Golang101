56b676db571138c1a57b9d987f102e0f71dadd3b
MichaelChen <helloworldsmart3@gmail.com>
2024-01-15 23:17:20 +0800

    feat: Using goCSV to Create a CSV File

--- Tutorials/CSVTutorial/How To Work With CSV Files In Go/main.go
+++ Tutorials/CSVTutorial/How To Work With CSV Files In Go/main.go
@@ -1,53 +1,77 @@
 package main
 
 import (
-	"encoding/csv"
+	"github.com/gocarina/gocsv"
 	"os"
 )
 
-func main() {
-	// Open the CSV file
-	//file, err := os.Open("data.csv")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer file.Close()
-	//
-	//// Read the CSV data
-	//reader := csv.NewReader(file)
-	//reader.FieldsPerRecord = -1 // Allow variable number of fields
-	//data, err := reader.ReadAll()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//// Print the CSV data
-	//for _, row := range data {
-	//	for _, col := range row {
-	//		fmt.Printf("%s,", col)
-	//	}
-	//	fmt.Println()
-	//}
+type Person struct {
+	Name   string `csv:"name"`
+	Age    int    `csv:"age"`
+	Gender string `csv:"gender"`
+}
 
-	// Write the CSV data
-	file2, err := os.Create("data1.csv")
+func main() {
+	file, err := os.Create("data2.csv")
 	if err != nil {
 		panic(err)
 	}
-	defer file2.Close()
+	defer file.Close()
 
-	writer := csv.NewWriter(file2)
-	defer writer.Flush()
-	// this defines the header value and data values for the new csv file
-	headers := []string{"name", "age", "gender"}
-	data1 := [][]string{
-		{"Alice", "25", "Female"},
-		{"Bob", "30", "Male"},
-		{"Charlie", "35", "Male"},
+	people := []*Person{
+		{"Alice", 25, "Female"},
+		{"Bob", 30, "Male"},
+		{"Charlie", 35, "Male"},
 	}
 
-	writer.Write(headers)
-	for _, row := range data1 {
-		writer.Write(row)
+	if err := gocsv.MarshalFile(&people, file); err != nil {
+		panic(err)
 	}
 }
+
+//func main() {
+//	// Open the CSV file
+//	//file, err := os.Open("data.csv")
+//	//if err != nil {
+//	//	panic(err)
+//	//}
+//	//defer file.Close()
+//	//
+//	//// Read the CSV data
+//	//reader := csv.NewReader(file)
+//	//reader.FieldsPerRecord = -1 // Allow variable number of fields
+//	//data, err := reader.ReadAll()
+//	//if err != nil {
+//	//	panic(err)
+//	//}
+//	//
+//	//// Print the CSV data
+//	//for _, row := range data {
+//	//	for _, col := range row {
+//	//		fmt.Printf("%s,", col)
+//	//	}
+//	//	fmt.Println()
+//	//}
+//
+//	// Write the CSV data
+//	file2, err := os.Create("data1.csv")
+//	if err != nil {
+//		panic(err)
+//	}
+//	defer file2.Close()
+//
+//	writer := csv.NewWriter(file2)
+//	defer writer.Flush()
+//	// this defines the header value and data values for the new csv file
+//	headers := []string{"name", "age", "gender"}
+//	data1 := [][]string{
+//		{"Alice", "25", "Female"},
+//		{"Bob", "30", "Male"},
+//		{"Charlie", "35", "Male"},
+//	}
+//
+//	writer.Write(headers)
+//	for _, row := range data1 {
+//		writer.Write(row)
+//	}
+//}
