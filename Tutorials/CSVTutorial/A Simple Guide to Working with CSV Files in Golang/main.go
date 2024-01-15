8f03fe3b9e36a71633f6058a94d3697daeb885db
MichaelChen <helloworldsmart3@gmail.com>
2024-01-15 21:02:18 +0800

    feat: A Simple Guide to Working with CSV Files in Golang

---
+++ Tutorials/CSVTutorial/A Simple Guide to Working with CSV Files in Golang/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("data.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = -1
+
+	csvData, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, each := range csvData {
+		fmt.Println(each)
+	}
+
+	//
+	for {
+		record, err := reader.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(record)
+	}
+
+}
