2ca1602c186c17385bcee4caa891a32322423a05
MichaelChen <helloworldsmart3@gmail.com>
2024-01-27 14:25:41 +0800

    feat: Slices of slice

--- DocTutorials/ATourOfGo/playground.go
+++ DocTutorials/ATourOfGo/playground.go
@@ -243,3 +243,21 @@ func tutorial18() {
 	var s []int = primes[1:4]
 	fmt.Println(s)
 }
+
+// Slices of slice
+
+func tutorial19() {
+	// Create a tic-tac-toe board.
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+
+	// The players take turns.
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
+}
