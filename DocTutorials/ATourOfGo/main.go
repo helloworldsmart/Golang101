01dcf64be8ec6a9163f59e3c4f0389fa23249590
MichaelChen <helloworldsmart3@gmail.com>
2024-01-28 17:01:40 +0800

    feat: Exercise: Fibonacci closure

--- DocTutorials/ATourOfGo/main.go
+++ DocTutorials/ATourOfGo/main.go
@@ -137,7 +137,12 @@ func tutorial28() {
 
 // Exercise: Fibonacci closure
 func fibonacci() func() int {
-
+	a, b := 0, 1
+	return func() int {
+		ret := a
+		a, b = b, a+b
+		return ret
+	}
 }
 
 func tutorial29() {
